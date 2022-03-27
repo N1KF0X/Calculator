b411b1142189502e5fa2b3a86ab6201275c6b8d9
N1KF0X <96484000+N1KF0X@users.noreply.github.com>
2022-03-27 18:53:44 +0300

    add files

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.calculator;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean choiceATextView = true;
+    boolean numbersMode = false;
+    Integer firstNumber = 0;
+    Integer secondNumber = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void changeNumbersMode(View view){
+       numbersMode =!numbersMode;
+        if(numbersMode){
+            if (choiceATextView){
+                TextView textView = findViewById(R.id.textView);
+                textView.setText("-"+firstNumber);
+            }
+            else{
+                TextView textView1 = findViewById(R.id.textView1);
+                textView1.setText("-"+secondNumber);
+            }
+        }
+        else {
+            if (choiceATextView){
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(firstNumber.toString());
+            }
+            else{
+                TextView textView1 = findViewById(R.id.textView1);
+                textView1.setText(secondNumber.toString());
+            }
+        }
+    }
+
+
+    public void onClickTextView(View view) {
+        choiceATextView = true;
+    }
+
+    public void onClickTextView1(View view) {
+        choiceATextView = false;
+    }
+
+    public void onClickButton(View view) {
+        Button button = findViewById(view.getId());
+        if(numbersMode){
+            if (choiceATextView){
+                TextView textView = findViewById(R.id.textView);
+                firstNumber = Integer.parseInt(firstNumber.toString()+button.getText());
+                textView.setText("-"+firstNumber);
+            }
+            else{
+                TextView textView1 = findViewById(R.id.textView1);
+                secondNumber = Integer.parseInt(secondNumber.toString()+button.getText());
+                textView1.setText("-"+secondNumber);
+            }
+        }
+        else {
+            if (choiceATextView){
+                TextView textView = findViewById(R.id.textView);
+                firstNumber = Integer.parseInt(firstNumber.toString()+button.getText());
+                textView.setText(firstNumber.toString());
+            }
+            else{
+                TextView textView1 = findViewById(R.id.textView1);
+                secondNumber = Integer.parseInt(secondNumber.toString()+button.getText());
+                textView1.setText(secondNumber.toString());
+            }
+        }
+    }
+
+    public void onClickResult(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        TextView textView = findViewById(R.id.textView);
+        TextView textView1 = findViewById(R.id.textView1);
+        intent.putExtra("firstNumber",Integer.parseInt(textView.getText().toString()));
+        intent.putExtra("secondNumber",Integer.parseInt(textView1.getText().toString()));
+        startActivity(intent);
+    }
+
+    public void onClickClear(View view){
+        if (choiceATextView){
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("");
+        }
+        else{
+            TextView textView1 = findViewById(R.id.textView1);
+            textView1.setText("");
+        }
+    }
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity2.java
@@ -0,0 +1,39 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    Intent intent = getIntent();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Bundle arguments = getIntent().getExtras();
+        TextView textView2 = findViewById(R.id.textView2);
+        String firstNumber;
+        String secondNumber;
+        Integer result = arguments.getInt("firstNumber")+arguments.getInt("secondNumber");
+        if(arguments.getInt("firstNumber")<0){
+           firstNumber = "("+arguments.get("firstNumber").toString()+")";
+        }
+        else {
+            firstNumber = arguments.get("firstNumber").toString();
+        }
+
+        if(arguments.getInt("secondNumber")<0){
+            secondNumber = "("+arguments.get("secondNumber").toString()+")";
+        }
+        else {
+            secondNumber = arguments.get("secondNumber").toString();
+        }
+        textView2.setText(firstNumber+" + "+secondNumber+" = "+result);
+    }
+
+
+}
